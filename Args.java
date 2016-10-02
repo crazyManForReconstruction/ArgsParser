3869edb1f859ae1583dd9cba0a22aaca803aa3f7
crazyManForReconstruction <xuxiaoping1990@126.com>
2016-10-02 11:00:37 +0800

    Update Args.java
    既支持Boolean又支持String类型

--- Args.java
+++ Args.java
@@ -1,4 +1,6 @@
+import java.text.ParseException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -6,53 +8,82 @@ import java.util.TreeSet;
 public class Args{
 	private String schema;
 	private String[] args;
-	private boolean valid;
+	private boolean valid = true;
 	private Set<Character> unexpectedArguments = new TreeSet<Character>();
 	private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
-	private int numberOfArguments = 0;
 	
-	public Args(String schema, String[] args){
+	private Set<Character> argsFound = new HashSet<Character>();
+	private Map<Character, String> stringArgs = new HashMap<Character, String>();
+	private int currentArgument = 0;
+	private char errorArgument = '\0';
+	
+	enum ErrorCode{
+		OK, MISSING_STRING
+	}
+	
+	private ErrorCode errorCode = ErrorCode.OK;
+	
+	public Args(String schema, String[] args) throws ParseException{
 		this.schema = schema;
 		this.args = args;
 		valid = parse();
 	}
 	
-	public boolean isValid(){
-		return valid;
-	}
-	
-	private boolean parse(){
-		if(schema.length() == 0 && args.length == 0){
+	private boolean parse() throws ParseException{
+		if(schema.length()==0 && args.length==0){
 			return true;
 		}
 		parseSchema();
 		parseArguments();
-		return unexpectedArguments.size() == 0;
+		return valid;
 	}
 	
-	private boolean parseSchema(){
+	private boolean parseSchema() throws ParseException{
 		for(String element:schema.split(",")){
-			parseSchemaElement(element);
+			if(element.length()>0){
+				String trimmedElement = element.trim();
+				parseSchemaElement(trimmedElement);
+			}
 		}
-		
 		return true;
 	}
 	
-	private void parseSchemaElement(String element){
-		if(element.length() == 1){
-			parseBooleanSchemaElement(element);
+	private void parseSchemaElement(String element)throws ParseException{
+		char elementId = element.charAt(0);
+		String elementTail = element.substring(1);
+		validateSchemaElementId(elementId);
+		if(isBooleanSchemaElement(elementTail)){
+			parseBooleanSchemaElement(elementId);
+		}else if(isStringSchemaElement(elementTail)){
+			parseStringSchemaElement(elementId);
 		}
 	}
 	
-	private void parseBooleanSchemaElement(String element){
-		char c = element.charAt(0);
-		if(Character.isLetter(c)){
-			booleanArgs.put(c, false);
+	private void validateSchemaElementId(char elementId) throws ParseException{
+		if(!Character.isLetter(elementId)){
+			throw new ParseException("Bad character:" + elementId + "in Args format: " + schema, 0);
 		}
+	}
+	
+	private boolean isBooleanSchemaElement(String elementTail){
+		return elementTail.length() == 0;
+	}
+	
+	private void parseBooleanSchemaElement(char elementId){
+		booleanArgs.put(elementId, false);
+	}
+	
+	private boolean isStringSchemaElement(String elementTail){
+		return elementTail.equals("*");
+	}
+	
+	private void parseStringSchemaElement(char elementId){
+		stringArgs.put(elementId, "null string");
 	}
 	
 	private boolean parseArguments(){
-		for(String arg:args){
+		for(currentArgument=0; currentArgument<args.length; currentArgument++){
+			String arg = args[currentArgument];
 			parseArgument(arg);
 		}
 		return true;
@@ -71,53 +102,106 @@ public class Args{
 	}
 	
 	private void parseElement(char argChar){
-		if(isBoolean(argChar)){
-			numberOfArguments++;
-			setBooleanArg(argChar, true);
+		if(setArgument(argChar)){
+			argsFound.add(argChar);
 		}else{
 			unexpectedArguments.add(argChar);
+			valid = false;
 		}
 	}
 	
-	private void setBooleanArg(char argChar, boolean value){
-		booleanArgs.put(argChar, value);
+	private boolean setArgument(char argChar){
+		boolean set = true;
+		if(isBoolean(argChar)){
+			setBooleanArg(argChar, true);
+		}else if(isString(argChar)){
+			setStringArg(argChar, "null string");
+		}else{
+			set = false;
+		}
+		
+		return set;
 	}
 	
 	private boolean isBoolean(char argChar){
 		return booleanArgs.containsKey(argChar);
 	}
 	
+	private void setBooleanArg(char argChar, boolean value){
+		booleanArgs.put(argChar, value);
+	}
+	
+	private boolean isString(char argChar){
+		return stringArgs.containsKey(argChar);
+	}
+	
+	private void setStringArg(char argChar, String s){
+		currentArgument++;
+		try{
+			System.out.println("setStringArg: " + args[currentArgument]);
+			stringArgs.put(argChar, args[currentArgument]);
+		}catch(ArrayIndexOutOfBoundsException e){
+			valid = false;
+			errorArgument = argChar;
+			errorCode = ErrorCode.MISSING_STRING;
+		}
+	}
+	
 	public int cardinality(){
-		return numberOfArguments;
+		return argsFound.size();
 	}
 	
 	public String usage(){
-		if(schema.length() > 0){
+		if(schema.length()>0){
 			return "-[" + schema + "]";
 		}else{
 			return "";
 		}
 	}
 	
-	public String errorMessage(){
-		if(unexpectedArguments.size() > 0){
+	public String errorMessage() throws Exception{
+		if(unexpectedArguments.size()>0){
 			return unexpectedArgumentMessage();
 		}else{
-			return "";
+			switch(errorCode){
+			case MISSING_STRING:return String.format("Could not find string parameter for -%c.", errorArgument);
+			case OK:throw new Exception("TITL:should not get here.");
+			}
 		}
+		return "";
 	}
 	
 	private String unexpectedArgumentMessage(){
-		StringBuffer message = new StringBuffer("Argument(s) -");
+		StringBuffer message = new StringBuffer("Argument(s)  -");
 		for(char c:unexpectedArguments){
 			message.append(c);
 		}
-		message.append(" unexpected.");
+		message.append("  unexpected.");
 		
 		return message.toString();
 	}
 	
 	public boolean getBoolean(char arg){
-		return booleanArgs.get(arg);
+		return falseIfNull(booleanArgs.get(arg));
+	}
+	
+	private boolean falseIfNull(Boolean b){
+		return b==null? false:b;
+	}
+	
+	public String getString(char arg){
+		return blankIfNull(stringArgs.get(arg));
+	}
+	
+	private String blankIfNull(String s){
+		return s==null? "":s;
+	}
+	
+	public boolean has(char arg){
+		return argsFound.contains(arg);
+	}
+	
+	public boolean isValid(){
+		return valid;
 	}
 }
